fd025cdd22c38fdce20e56bc69f53bfe4888873e
Sam Waters <samuel.waters@gmail.com>
2023-08-02 19:29:52 -0600

    chore: finished

--- app/debug.py
+++ app/debug.py
@@ -1,9 +1,63 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from models import Company, Dev
+from models import Company, Dev, Freebie
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///freebies.db')
-    import ipdb; ipdb.set_trace()
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(Company).delete()
+    session.query(Dev).delete()
+    session.query(Freebie).delete()
+    session.commit()
+
+    company = Company(name="Flatiron", founding_year=2025)
+    session.add(company)
+    session.commit()
+
+    company2 = Company(name="Acre", founding_year=2005)
+    session.add(company2)
+    session.commit()
+
+    company3 = Company(name="Thirma", founding_year=2012)
+    session.add(company3)
+    session.commit()
+
+    dev = Dev(name="Alice")
+    session.add(dev)
+    session.commit()
+
+    freebie = Freebie(item_name="T-shirt", value=10, company_id=company.id, dev_id=dev.id)
+    session.add(freebie)
+    session.commit()
+
+    oldest = Company.oldest_company(session)
+    print(oldest)
+
+    company.give_freebie( dev, "T-shirt", 10)
+    company.give_freebie( dev, "Mug", 5)
+
+    print(dev.received_one("T-shirt"))  # Should print: True
+    print(dev.received_one("Mug"))  # Should print: True
+    print(dev.received_one("Sticker"))  # Should print: False
+
+    companies = session.query(Company).all()
+    for company in companies:
+        print(company)
+
+    devs = session.query(Dev).all()
+    for dev in devs:
+        print(dev)
+
+    freebies = session.query(Freebie).all()
+    for freebie in freebies:
+        print(freebie)
+
+    session.close()
+    # import ipdb; 
+    # ipdb.set_trace()
--- app/models.py
+++ app/models.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import (Column, String, Integer, ForeignKey)
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+
+
+engine = create_engine('sqlite:///freebies.db')
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 Base = declarative_base()
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name= Column(String())
+    value = Column(Integer())
+    company_id = Column(Integer, ForeignKey('companies.id'))
+    company = relationship('Company', back_populates='freebies')
+    dev_id = Column(Integer, ForeignKey('devs.id'))
+    dev = relationship('Dev', back_populates='freebies')
+
+    def __repr__(self):
+        return f'<Freebie {self.item_name}, {self.value}, {self.dev.name}, {self.company.name}>'
+    
+    def print_details(self):
+        return f'{self.dev.name} owns a {self.item_name} from {self.company.name}.'
+    
 
 class Company(Base):
     __tablename__ = 'companies'
@@ -11,15 +37,38 @@ class Company(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     founding_year = Column(Integer())
+    # Relationship with Freebie
+    freebies = relationship('Freebie', back_populates='company')
+
 
     def __repr__(self):
         return f'<Company {self.name}>'
+    
+    def give_freebie(self, dev, item_name, value):
+        freebie = Freebie(item_name=item_name, value=value, company_id=self.id, dev_id=dev.id)
+        session.add(freebie)
+        session.commit()
+        session.close()
+        return freebie
+    
+    @classmethod
+    def oldest_company(cls, session):
+        return session.query(cls).order_by(cls.founding_year).first()
+    
 
 class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
     name= Column(String())
+    # Relationship with Freebie
+    freebies = relationship('Freebie', back_populates='dev')
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+    
+    def received_one(self, item_name):
+        for freebie in self.freebies:
+            if freebie.item_name == item_name:
+                return True
+        return False
